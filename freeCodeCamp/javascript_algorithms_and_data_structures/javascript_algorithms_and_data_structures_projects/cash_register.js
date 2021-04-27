000cb757d4e6c3e75cc50090233264953b90d327
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-04-27 03:51:35 -0700

    improve algorithm

--- freeCodeCamp/javascript_algorithms_and_data_structures/javascript_algorithms_and_data_structures_projects/cash_register.js
+++ freeCodeCamp/javascript_algorithms_and_data_structures/javascript_algorithms_and_data_structures_projects/cash_register.js
@@ -1,124 +1,71 @@
-function checkCashRegister(price, cash, cid) {
-    const cidSum = cid
-        .map(i => i[1])
-        .reduce((a,b) => a + b)
+const denomVal = { "ONE HUNDRED": 100.0, "FIFTY": 50.0, "TWENTY": 20.0, "TEN": 10.0, "FIVE": 5.0, "ONE": 1.0, "QUARTER": 0.25, "DIME": 0.10, "NICKEL": 0.05, "PENNY": 0.01 };
 
-    if (cidSum < cash - price) {
+function checkCashRegister(price, cash, cid) {
+    let due = cash - price;
+
+    const cidSum = Math.round(cid // use Math.round to fix rounding error
+      .map(([,b]) => b)
+      .reduce((a,b) => a + b) * 100) / 100;
+
+    if (cidSum < due) {
         return {
             "status": "INSUFFICIENT_FUNDS",
             "change": []
-        }
+        };
     }
 
-    if (cidSum == cash - price) {
+    if (cidSum === due) {
         return {
             "status": "CLOSED",
             "change": cid
-        }
+        };
     }
 
-    let change = [
-        ["ONE HUNDRED", 0],
-        ["TWENTY", 0],
-        ["TEN", 0],
-        ["FIVE", 0],
-        ["ONE", 0],
-        ["QUARTER", 0],
-        ["DIME", 0],
-        ["NICKEL", 0],
-        ["PENNY", 0],
-    ]
+    let change = cid
+        .sort(() => -1)
+        .map(([denom,]) => [denom,0])
+        .map(([denom,]) => {
 
-    let tmp = cash - price
-    change = change.map(([denom,]) => {
+            let wanted = Math.floor(due / denomVal[denom]) * denomVal[denom];
+            const [,avail] = cid.find(([x,]) => x === denom);
+            wanted = (wanted < avail) ? wanted : avail;
+            due -= wanted;
+            due = Math.round(due * 100) / 100; // use Math.round to fix rounding error
 
-        let val
-        switch (denom) {
-            case "ONE HUNDRED":
-                val = 100
-                break
-            case "FIFTY":
-                val = 50
-                break
-            case "TWENTY":
-                val = 20
-                break
-            case "TEN":
-                val = 10
-                break
-            case "FIVE":
-                val = 5
-                break
-            case "ONE":
-                val = 1
-                break
-            case "QUARTER":
-                val = 0.25
-                break
-            case "DIME":
-                val = 0.10
-                break
-            case "NICKEL":
-                val = 0.05
-                break
-            case "PENNY":
-                val = 0.01
-                break
-        }
+            return [
+                denom,
+                wanted
+            ];
 
-        let avail = cid.filter(i => i[0] === denom)[0][1]
-        let wanted = Math.floor(tmp / val) * val
-        wanted = (wanted < avail) ? wanted : avail
-        tmp -= wanted
+        }).filter(([,x]) => x > 0);
 
-        return [
-            denom,
-            wanted
-        ]
-    }).filter(([,amt]) => amt > 0)
 
-    if (tmp > 0) {
+    if (due > 0) {
         return {
-            status: "INSUFFICIENT_FUNDS",
-            change: []
-        }
+            "status": "INSUFFICIENT_FUNDS",
+            "change": []
+        };
     } else {
         return {
-            status: (price < cidSum) ? "OPEN" : "CLOSED",
-            change: change
-        }
+            "status": "OPEN",
+            "change": change
+        };
     }
 }
 
-console.log("===== tests ====="); //^
-const qsAndAs = [
+console.log("=== tests ======================================================");
 
-    {q: checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]), a: {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}}
-
+const qa = [
     // {q: checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]), a: {status: "OPEN", change: [["QUARTER", 0.5]]}},
-
-    // {q: checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]), a: {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}},
-
-    // {q: checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]), a: {status: "INSUFFICIENT_FUNDS", change: []}},
-
-    // {q: checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]), a: {status: "INSUFFICIENT_FUNDS", change: []}},
-
-    // {q: checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]), a: {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}},
-
+    {q: checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]), a: {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}},
+    // {q: checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+    // {q: checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}}
+    // {q: checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
 ]
 
-qsAndAs.forEach(({q,a}) => {
-
-    console.log(a);
-
-    // if (q === a) {
-    //     result = "OOO PASS"
-    // } else {
-    //     result = "XXX FAIL"
-    // }
-    // console.log(result + " | " + q + " | " + a);
-
+qa.forEach(({q,a}) => {
+    // console.log(q)
+    console.log(a)
 })
-//$
 
 // vim: fdm=marker fmr=//^,//$
